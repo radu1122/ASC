8170f93d39b8c03b2e6b7a2e6b33a3692db34d59
Radu Dum laptop <d.raduandrei@gmail.com>
2022-04-29 00:01:16 +0300

    added neopt

--- tema2/src/solver_neopt.c
+++ tema2/src/solver_neopt.c
@@ -45,8 +45,8 @@ double* my_solver(int N, double *A, double* B) {
 	memset(BXA, 0, N * N * sizeof(double));
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			// k will go only to i because A is triangular
-			for (int k = 0; k < N; k++) {
+			// k will go only to j because A is triangular
+			for (int k = 0; k <= j; k++) {
 				BXA[i * N + j] = BXA[i * N + j] + B[i * N + k] * A[k * N + j];
 			}
 		}
