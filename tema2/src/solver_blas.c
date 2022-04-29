c3ba8c27c93e857da219d714698eed9e842d5f9a
Radu Dum laptop <d.raduandrei@gmail.com>
2022-04-29 11:53:49 +0300

    added neopt

--- tema2/src/solver_blas.c
+++ tema2/src/solver_blas.c
@@ -8,17 +8,39 @@
  * Add your BLAS implementation here
  */
 double* my_solver(int N, double *A, double *B) {
-	printf("BLAS SOLVER\n");
 
-	// A transpose with BLAS
-	// double *AT = (double*)malloc(N * N * sizeof(double));
-	// memset(AT, 0, N * N * sizeof(double));
-	// dtrans(N, N, A, N, AT, N);
+	// BTXB
+	double *BTXB = calloc(N * N, sizeof(double));
+	memset(BTXB, 0, N * N * sizeof(double));
+	cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, N, N, N, 1, B, N, B, N, 1, BTXB, N);
 
-	// // B transpose with BLAS function
-	// double *BT = (double*)malloc(N * N * sizeof(double));
-	// memset(BT, 0, N * N * sizeof(double));
-	// dtrans(N, N, B, N, BT, N);
-	return NULL;
+	// BXA
+	double *BXA = calloc(N * N, sizeof(double));
+	memset(BXA, 0, N * N * sizeof(double));
+	cblas_dcopy(N * N, B, 1, BXA, 1);
+	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasTrans, CblasNonUnit, N, N, 1, A, N, BXA, N);
+	
+	//BXAXAT
+	double *BXAXAT = calloc(N * N, sizeof(double));
+	memset(BXAXAT, 0, N * N * sizeof(double));
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1, BXA, N, A, N, 1, BXAXAT, N);
+
+	// BXAXAT + BTXB
+	double *result = (double*)malloc(N * N * sizeof(double));
+	memset(result, 0, N * N * sizeof(double));
+	cblas_dcopy(N * N, BTXB, 1, result, 1);
+
+	// 0 buffer
+	double *oneMatrix = calloc(N * N, sizeof(double));
+	memset(oneMatrix, 1, N * N * sizeof(double));
+
+	cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, N, N, N, 1, BXAXAT, N, oneMatrix, N, 1, result, N);
+
+	free(BTXB);
+	free(BXA);
+	free(BXAXAT);
+	free(oneMatrix);
+
+	return result;
 
 }
